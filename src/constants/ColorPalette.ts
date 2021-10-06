9ed13a3d2c0e9c949b593f9945945ee53c42be4d
reeq20 <rikusugawara@fabrec.jp>
2021-10-06 20:50:48 +0900

    rename typo

---
+++ src/constants/ColorPalette.ts
@@ -0,0 +1,14 @@
+export const COLOR_PALETTE = Object.freeze({
+        orange: '#f36d1c',
+        sunyellow: '#ffdf1e',
+        poisongreen: '#61ff21',
+        aquamarine: '#17ffb1',
+        brightlightblue: '#21d0ff',
+        ceruleanblue: '#1269e6',
+        vividpurple: '#7800ff',
+        electricpurple: '#ae2dfd',
+        pinkred: '#ed1c66',
+        pinkpurple: '#f026ff',
+        skyblue: '#12cde6'
+    }
+)
